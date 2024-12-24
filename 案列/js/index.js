872b127c522491b3b2c9daf2e805c55715c863b3
aliyuv <1324149139@qq.com>
2024-12-24 16:08:33 +0800

    rem

---
+++ 案列/js/index.js
@@ -0,0 +1,16 @@
+(function () {
+    const setFontSize = () => {
+        //获取html元素
+        const html = document.documentElement
+        //获取设备的宽度
+        const deviceWidth = parseInt(html.getBoundingClientRect().width)
+        //如果超过最大宽度的值那么就使用基准值
+        deviceWidth > 750 ? 750 : deviceWidth
+        //设置html的字体大小
+        html.style.fontSize = (100 / 375) * deviceWidth + 'px'
+    }
+    //初始化
+    setFontSize()
+    //监听窗口变化
+    window.addEventListener('resize', setFontSize)
+})()
