5c161b01292e4a688e6caacde090babf15e99fcc
aliyuv <1324149139@qq.com>
2024-12-25 14:34:39 +0800

    优化

--- rem/js/index.js
+++ rem/js/index.js
@@ -1,16 +1,13 @@
 (function () {
     const setFontSize = () => {
-        //获取html元素
         const html = document.documentElement
-        //获取设备的宽度
-        const deviceWidth = parseInt(html.getBoundingClientRect().width)
-        //如果超过最大宽度的值那么就使用基准值
-        deviceWidth > 750 ? 750 : deviceWidth
-        //设置html的字体大小
-        html.style.fontSize = (100 / 375) * deviceWidth + 'px'
+        const deviceWidth = Math.min(html.clientWidth, 750)
+        // (deviceWidth / 375) * 100，与原公式 (100 / 375) * deviceWidth 等价
+        html.style.fontSize = (deviceWidth / 375) * 100 + 'px'
     }
-    //初始化
+
+    // 初始化
     setFontSize()
-    //监听窗口变化
+    // 监听窗口变化
     window.addEventListener('resize', setFontSize)
 })()
